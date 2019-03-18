1c8a335ba66b018040837233589d9a686249c20a
Pablo Aravena <paravena74@gmail.com>
2019-03-18 10:12:13 -0300

    some changes

--- server/src/client/components/Home.js
+++ server/src/client/components/Home.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>I'm the home good 2 component</div>;
+  return (
+    <div>
+      <div>I'm the home good component</div>
+      <button onClick={() => console.log('Hi There!')}>Press Me</button>
+    </div>
+  );
 }
 
 export default Home;
